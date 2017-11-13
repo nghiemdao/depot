c20bfe07a12bc6f1129a909d744f5a37395a0468
nghiemdao <nghiemvandao@gmail.com>
2017-11-13 14:49:50 +0900

    Update test case for authorizing user

--- spec/controllers/admin_controller_spec.rb
+++ spec/controllers/admin_controller_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe AdminController, type: :controller do
-
   describe "GET #index" do
     setup
 
     it "returns http success" do
       get :index
+
       expect(response).to have_http_status(:success)
     end
   end
 
+  it "should not access without login" do
+    get :index
+    
+    expect(response).to redirect_to login_url
+  end
 end
