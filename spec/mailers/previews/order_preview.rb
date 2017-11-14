2bde145735f3b0700194ea3caef4cd3a0ce31695
nghiemdao <nghiemvandao@gmail.com>
2017-11-14 13:35:00 +0900

    Update mailer templates and set previews

--- spec/mailers/previews/order_preview.rb
+++ spec/mailers/previews/order_preview.rb
@@ -3,12 +3,16 @@ class OrderPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/order/received
   def received
-    OrderMailer.received
+    order = Order.last
+
+    OrderMailer.received(order)
   end
 
   # Preview this email at http://localhost:3000/rails/mailers/order/shipped
   def shipped
-    OrderMailer.shipped
+    order = Order.last
+
+    OrderMailer.shipped(order)
   end
 
 end
